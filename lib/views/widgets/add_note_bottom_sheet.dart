2b63142fa049ebb9a64246147ca893cf2d695157
Ahmed Ismail <ahmedismaeel064@gmail.com>
2025-09-16 09:39:17 +0300

    add fields to notes bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+      child: Column(
+        children: [
+          CustomTextField(
+            hintText: 'Title',
+          ),
+          CustomTextField(
+            hintText: 'Content',
+            maxLines: 5,
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -2,18 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constant.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hintText});
+  const CustomTextField({super.key, required this.hintText, this.maxLines = 1});
 
   final String hintText;
+  final int maxLines;
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      cursorColor: kPrimaryColor,
-      decoration: InputDecoration(
-        hintText: hintText,
-        border: buildBorder(),
-        enabledBorder: buildBorder(),
-        focusedBorder: buildBorder(color: kPrimaryColor),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: TextField(
+        cursorColor: kPrimaryColor,
+        maxLines: maxLines,
+        decoration: InputDecoration(
+          hintText: hintText,
+          border: buildBorder(),
+          enabledBorder: buildBorder(),
+          focusedBorder: buildBorder(color: kPrimaryColor),
+        ),
       ),
     );
   }
