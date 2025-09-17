423bea6225fdeead4007351986004d3c81412b57
Ahmed Ismail <ahmedismaeel064@gmail.com>
2025-09-17 09:16:28 +0300

    create custom button

---
+++ lib/views/widgets/custom_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constant.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.text});
+
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 48,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: kPrimaryColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+}
